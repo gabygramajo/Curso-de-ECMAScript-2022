b12ee48cc891e2f0187509a90d6d041a853e92e8
gaby gramajo <ggabrieldeveloper@gmail.com>
2022-11-12 18:39:29 -0300

    add es13

---
+++ src/es13/00-at.js
@@ -0,0 +1,9 @@
+
+const array = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+// acceder al ultimo item del array antes de es13
+console.log( array[array.length - 1] ); 
+// >> six
+
+console.log( array.at(-1) ); 
+// >> six
